f32875ea906616250771f9dfdede3c09825bf0a6
luthfi070 <luthfimusafa@gmail.com>
2024-04-01 02:26:27 +0700

    added info

--- src/App.tsx
+++ src/App.tsx
@@ -88,7 +88,7 @@ function App() {
 
   return (
     <div className='App mt-5'>
-      <div className='container'>
+      <div className='container fluid'>
         <div className='row'>
           <div className='col'>
             {loading ? (
@@ -118,58 +118,71 @@ function App() {
             )}
           </div>
           <div className='col'>
-            <Form>
-              <Form.Group className='mb-3'>
-                <Form.Label className='align-left'>Clinic Name</Form.Label>
-                <Form.Control
-                  name='name'
-                  type='text'
-                  placeholder='Enter clinic name'
-                  onChange={handleChange}
-                />
-              </Form.Group>
-              <Form.Group className='mb-3'>
-                <Form.Label>Time Open</Form.Label>
-                <Form.Control
-                  name='open'
-                  type='text'
-                  placeholder='ex : 10:30'
-                  onChange={handleChange}
-                />
-              </Form.Group>
-              <Form.Group className='mb-3'>
-                <Form.Label>Time Close</Form.Label>
-                <Form.Control
-                  name='close'
-                  type='text'
-                  placeholder='ex : 10:30'
-                  onChange={handleChange}
-                />
-              </Form.Group>
-              <Form.Group className='mb-3'>
-                <Form.Label>
-                  How long time for each client ? ( in minutes )
-                </Form.Label>
-                <Form.Control
-                  name='timeSlot'
-                  type='text'
-                  placeholder='ex : 15 '
-                  onChange={handleChange}
-                />
-              </Form.Group>
-              <Form.Group className='mb-3'>
-                <Form.Label>
-                  How many client are you taking per day ?
-                </Form.Label>
-                <Form.Control
-                  name='quota'
-                  type='text'
-                  placeholder='ex : 15 '
-                  onChange={handleChange}
-                />
-              </Form.Group>
-              <Button onClick={() => createClinic()}>Submit</Button>
-            </Form>
+            <div className='row '>
+              {" "}
+              <Form>
+                <Form.Group className='mb-3'>
+                  <Form.Label className='align-left'>Clinic Name</Form.Label>
+                  <Form.Control
+                    name='name'
+                    type='text'
+                    placeholder='Enter clinic name'
+                    onChange={handleChange}
+                  />
+                </Form.Group>
+                <Form.Group className='mb-3'>
+                  <Form.Label>Time Open</Form.Label>
+                  <Form.Control
+                    name='open'
+                    type='text'
+                    placeholder='ex : 10:30'
+                    onChange={handleChange}
+                  />
+                </Form.Group>
+                <Form.Group className='mb-3'>
+                  <Form.Label>Time Close</Form.Label>
+                  <Form.Control
+                    name='close'
+                    type='text'
+                    placeholder='ex : 10:30'
+                    onChange={handleChange}
+                  />
+                </Form.Group>
+                <Form.Group className='mb-3'>
+                  <Form.Label>
+                    How long time for each client ? ( in minutes )
+                  </Form.Label>
+                  <Form.Control
+                    name='timeSlot'
+                    type='text'
+                    placeholder='ex : 15 '
+                    onChange={handleChange}
+                  />
+                </Form.Group>
+                <Form.Group className='mb-3'>
+                  <Form.Label>
+                    How many client are you taking per day ?
+                  </Form.Label>
+                  <Form.Control
+                    name='quota'
+                    type='text'
+                    placeholder='ex : 15 '
+                    onChange={handleChange}
+                  />
+                </Form.Group>
+                <Button onClick={() => createClinic()}>Submit</Button>
+              </Form>
+            </div>
+            <div className='row-xl mt-3 mb-3'>Tech Used</div>
+            <div className='row-xl'>
+              Backend : NodeJS, Deployed on <b>Google Cloud</b>
+            </div>
+            <div className='row-xl'>
+              Frontend : Typescript React, Deployed on <b>Netlify</b>
+            </div>
+            <div className='row-xl'>
+              Database : MongoDB, Deployed on <b>MongoDB Atlas</b>
+            </div>
           </div>
         </div>
       </div>
